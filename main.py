efaaff67779044c023c0ed8e29bc8d1bb4233746
eric workman <workman.eric93@gmail.com>
2022-06-27 17:42:26 -0500

    Updated to include project to main file

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print('hello world')
+import random
 
+destinations = ["China", "Luxembourg", "Beloit"]
+
+def random_item_picker(list):
+    random_item = random.choice(list)
+    return random_item
+
+def user_destination ():
+    random_destinaitons = random_item_picker(destinations)
+    user_input = input(f'would you like to go to {random_destinaitons}  y/n ')
+    if user_input == "y":
+        print(f'You have confirmed that you would like to stay in {random_destinaitons}')
+    elif user_input == "n":
+        return user_destination()
+confirmed_destination = user_destination()
