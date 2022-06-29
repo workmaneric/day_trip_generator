4b66debb022b3c0764e9180779c2b9442a69087c
workmaneric <107960744+workmaneric@users.noreply.github.com>
2022-06-28 19:23:41 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import random
 
-destinations = ["China", "Luxembourg", "Beloit"]
+destinations = ["China", "Luxembourg", "Beloit", "Canada", "Saginaw", "Antarctica"]
+restaurants = ["Chilis", "Subway", "Mcdonalds", "In n Out", "KFC" ]
+transport = ["Car", "Bicycle", "Unicycle", "Horse", "Segway", "Jet Pack"]
+entertainment = ["Museum", "Music festival", "Yacht race", "Chill walk in the park", "Wine Bar" ]
+
+print("Lets plan a day trip!")
 
 def random_item_picker(list):
     random_item = random.choice(list)
     return random_item
 
-def user_destination ():
-    random_destinaitons = random_item_picker(destinations)
-    user_input = input(f'would you like to go to {random_destinaitons}  y/n ')
+def user_selection (list):
+    random_selection = random_item_picker(list)
+    user_input = input(f'Would you like {random_selection}?  y/n ')
     if user_input == "y":
-        print(f'You have confirmed that you would like to stay in {random_destinaitons}')
+        print(f'You have confirmed that you would like {random_selection}')
+        return random_selection
     elif user_input == "n":
-        return user_destination()
-confirmed_destination = user_destination()
+        return user_selection(list)
+confirmed_destination = user_selection(destinations)
+confirmed_restaurant = user_selection(restaurants)
+confirmed_transport = user_selection(transport)
+confirmed_entertainment = user_selection(entertainment)
+print(f'You will be traveling to {confirmed_destination} where you will eat the finest cuisine at {confirmed_restaurant} traveling by {confirmed_transport} and then relaxing by going to a {confirmed_entertainment}')
+
